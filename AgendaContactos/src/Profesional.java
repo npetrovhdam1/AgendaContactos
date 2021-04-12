c635ca1aaf23dca6525204b8bcc5279aa136616e
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-12 18:14:14 +0200

    metodo capitalizeString terminado (con duda)

--- AgendaContactos/src/Profesional.java
+++ AgendaContactos/src/Profesional.java
@@ -7,3 +7,19 @@ public class Profesional extends Contacto {
 		super(nombre, apellidos, telefono, email);
 		this.empresa = capitalizeString(empresa);
 	}
+	// https://stackoverflow.com/questions/1892765/how-to-capitalize-the-first-character-of-each-word-in-a-string
+		//He mirado en esta página y creo que se puede hacer asi(no se como podría hacerlo de otra manera)
+		public static String capitalizeString(String string) {
+			char[] chars = string.toLowerCase().toCharArray();
+			boolean found = false;
+			for (int i = 0; i < chars.length; i++) {
+				if (!found && Character.isLetter(chars[i])) {
+					chars[i] = Character.toUpperCase(chars[i]);
+					found = true;
+				} else if (Character.isWhitespace(chars[i])) {
+					found = false;
+				}
+			}
+			return String.valueOf(chars);
+		}
+}
