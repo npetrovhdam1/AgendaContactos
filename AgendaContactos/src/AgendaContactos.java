e01b28389c37f5657761ce333f84c4c9ebf164e7
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-12 18:24:26 +0200

    Clase AgendaContactos modificada y terminada

--- AgendaContactos/src/AgendaContactos.java
+++ AgendaContactos/src/AgendaContactos.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class AgendaContactos {
@@ -11,7 +15,7 @@ public class AgendaContactos {
 
 	}
 
-	public void añadirContacto() {
+	public void añadirContacto(Contacto contacto) {
 
 	}
 
@@ -29,6 +33,10 @@ public class AgendaContactos {
 		return null;
 	}
 
+	/*
+	 * Devuelve una colección ArrayList con todos los contactos (personales o
+	 * profesionales) que incluyen el texto indicado en su nombre o apellidos.
+	 */
 	public List<Contacto> buscarContactos(String texto) {
 		List<Contacto> contactosEncontrados = new ArrayList<Contacto>();
 		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
@@ -42,8 +50,10 @@ public class AgendaContactos {
 		return contactosEncontrados;
 	}
 
-	}
-
+	/*
+	 * Dada una letra devuelve una colección ArrayList con los contactos personales
+	 * en esa letra. Si la letra no está en la agenda se devuelve null.
+	 */
 	public List<Personal> personalesEnLetra(char letra) {
 		List<Personal> personalesEncontrados = new ArrayList<Personal>();
 		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
@@ -62,9 +72,10 @@ public class AgendaContactos {
 		return personalesEncontrados;
 	}
 
-
-	}
-
+	/*
+	 * Devuelve una colección ArrayList con todos los contactos personales a los que
+	 * hay que felicitar.
+	 */
 	public List<Personal> felicitar() {
 		List<Personal> personalesAFelicitar = new ArrayList<Personal>();
 		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
@@ -80,52 +91,53 @@ public class AgendaContactos {
 		return personalesAFelicitar;
 	}
 
-
-	}
-
-	public void personalesPorRelacion() {
+	/*
+	 * Devuelve un nuevo map en el que aparecen solo contactos personales pero
+	 * organizados de forma que la clave en el nuevo map es la relación (un
+	 * enumerado) y el valor asociado una colección List de cadenas con los
+	 * apellidos y nombre de todos los contactos personales que hay en la agenda.
+	 * Las claves se recuperan en el orden natural del enumerado.
+	 */
+	public Map<Relacion, Set<String>> personalesPorRelacion() {
 		// Creamos nuevo mapa para albergar resultados
-				Map<Relacion, Set<String>> personalesRelacionados = new HashMap<Relacion, Set<String>>();
-				// recorremos los valores del enumerado relacion
-				for (Relacion relacion : Relacion.values()) {
-					// creamos nueva entrada para el mapa de resultados
-					Entry<Relacion, Set<String>> entrada = Map.entry(relacion, new HashSet<String>());
-					// recorremos las entradas de la agenda
-					for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
-						// recorremos los contactos de la entrada
-						for (Contacto contacto : entradaAgenda.getValue()) {
-							// preguntamos si el contacto es de tipo personal
-							if (contacto.getClass() == Personal.class) {
-								// convertimos el contact en contacot personal
-								Personal contactoPersonal = (Personal) contacto;
-								// preguntamos si la relacion del contacto personal es la relacion actual
-								if (contactoPersonal.getRelacion() == relacion) {
-									// obtenemos nombre y apellidos el contacto
-									String nombreApellidos = contactoPersonal.getNombre() + " , "
-											+ contactoPersonal.getApellidos();
-									// añadimos los nombre y apellidos a la coleccion de la entrada nueva
-									entrada.getValue().add(nombreApellidos);
-								}
-							}
+		Map<Relacion, Set<String>> personalesRelacionados = new HashMap<Relacion, Set<String>>();
+		// recorremos los valores del enumerado relacion
+		for (Relacion relacion : Relacion.values()) {
+			// creamos nueva entrada para el mapa de resultados
+			Entry<Relacion, Set<String>> entrada = Map.entry(relacion, new HashSet<String>());
+			// recorremos las entradas de la agenda
+			for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
+				// recorremos los contactos de la entrada
+				for (Contacto contacto : entradaAgenda.getValue()) {
+					// preguntamos si el contacto es de tipo personal
+					if (contacto.getClass() == Personal.class) {
+						// convertimos el contact en contacot personal
+						Personal contactoPersonal = (Personal) contacto;
+						// preguntamos si la relacion del contacto personal es la relacion actual
+						if (contactoPersonal.getRelacion() == relacion) {
+							// obtenemos nombre y apellidos el contacto
+							String nombreApellidos = contactoPersonal.getNombre() + " , "
+									+ contactoPersonal.getApellidos();
+							// añadimos los nombre y apellidos a la coleccion de la entrada nueva
+							entrada.getValue().add(nombreApellidos);
 						}
-
 					}
 				}
 
-				return personalesRelacionados;
 			}
+		}
 
+		return personalesRelacionados;
 	}
 
+	/*
+	 * Dada una letra devuelve los contactos personales ordenados por fecha de
+	 * nacimiento (ascendente)
+	 */
 	public List<Personal> personalesOrdenadosPorFechaNacimiento(char letra) {
 		List<Personal> personalesEncontrados = personalesEnLetra(letra);
 		 Collections.sort(personalesEncontrados, (Personal p1, Personal p2)-> p1.getFechaNacimiento().compareTo(p2.getFechaNacimiento()));
 		 return personalesEncontrados;
-
-	}
-
-}
-
 
 	}
 
