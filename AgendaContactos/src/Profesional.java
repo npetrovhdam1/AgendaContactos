e128381ec54851b759cd133288447f1129c6f6c8
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-12 18:12:27 +0200

    Constructor de la clase Pofesional creado

--- AgendaContactos/src/Profesional.java
+++ AgendaContactos/src/Profesional.java
@@ -1,9 +1,9 @@
+import java.util.Random;
 
-public class Profesional {
+public class Profesional extends Contacto {
+	private String empresa;
 
-	public Profesional() 
-	{
-		
+	public Profesional(String nombre, String apellidos, String telefono, String email, String empresa) {
+		super(nombre, apellidos, telefono, email);
+		this.empresa = capitalizeString(empresa);
 	}
-
-}
